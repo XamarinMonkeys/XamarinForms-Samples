5f7f1e8500a732e5947e9d888eeab97a04b403ca
Delpin Susai Raj <delpinsusairajm@rajasri.net>
2019-05-08 16:55:29 +0530

    Listview context action added

---
+++ XamarinList/XamarinList/XamarinList/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinList
+{
+    public partial class MainPage : ContentPage
+    {
+        string[] monkeys = new string[] { "Tamarins", "Capuchins", "Squirrel Monkeys", "Spider Monkeys" };
+        public MainPage()
+        {
+            InitializeComponent();
+            List<MonkeyList> monkeyLists = new List<MonkeyList>();
+            foreach (var monkey in monkeys)
+            {
+                MonkeyList monkeyList = new MonkeyList() { Name= monkey };
+                monkeyLists.Add(monkeyList);
+            }
+            lstMonkeys.ItemsSource = monkeyLists;
+        }
+
+        public void OnEdit(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            DisplayAlert("Edit", mi.CommandParameter.ToString(), "OK");
+        }
+
+        public void OnDelete(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            DisplayAlert("Delete", mi.CommandParameter.ToString(), "OK");
+        }
+    }
+}
---
+++ XamarinList/XamarinList/XamarinList/MonkeyList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinList
+{
+    public class MonkeyList
+    {
+        public string Name{ get; set; }
+    }
+}
